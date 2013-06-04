bf4be3d06843498e4d27421a89ea34c8f3c5f054
Piper Chester <piperchester@gmail.com>
2013-06-04 14:14:09 -0400

    Adding Storyboard, changing autoreleasepool call.

--- AccessLecture/AccessLectureAppDelegate.m
+++ AccessLecture/AccessLectureAppDelegate.m
@@ -10,38 +10,42 @@
  Customization after application launches
  */
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Initalize the root view controller (our home screen)
-    rootViewController = [[RootViewController alloc] initWithNibName:@"RootViewController" bundle:nil];
+
+    UINavigationController *rootNavigationController = (UINavigationController *)self.window.rootViewController;
+//    RootViewController *rootViewController = (RootViewController *)[rootNavigationController topViewController];
     
-    // Initialize the navigation cobtroller with the root view controller 
-    navigationController = [navigationController initWithRootViewController: rootViewController];
-    
-    // Set the application defaults
-    defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setFloat:0.0f forKey:@"toolbarAlpha"];
-    [defaults setFloat:0.5f forKey:@"userScrollSpeed"];
-    [defaults setFloat:0.3f forKey:@"tbRed"];
-    [defaults setFloat:0.3f forKey:@"tbGreen"];
-    [defaults setFloat:0.3f forKey:@"tbBlue"];
-    [defaults setFloat:1.0f forKey:@"textRed"];
-    [defaults setFloat:1.0f forKey:@"textGreen"];
-    [defaults setFloat:1.0f forKey:@"textBlue"];
-    [defaults setFloat:0.0145f forKey:@"userZoomIncrement"];
-    [defaults setFloat:.3 forKey:@"penSize"];
-    [defaults setFloat:.2 forKey:@"eraserSize"];
-    [defaults setValue:@"biology.png" forKey:@"testImage"];
-    [defaults synchronize];
-    
-    // Set the starting view
-    self.window.rootViewController = self.navigationController;
-    [self.window makeKeyAndVisible];
+    // Configure myViewController.
     
     return YES;
     
+    //    // Initalize the root view controller (our home screen)
+//    rootViewController = [[RootViewController alloc] initWithNibName:@"RootViewController" bundle:nil];
+//    
+//    // Initialize the navigation cobtroller with the root view controller 
+//    navigationController = [navigationController initWithRootViewController: rootViewController];
+//    
+//    // Set the application defaults
+//    defaults = [NSUserDefaults standardUserDefaults];
+//    [defaults setFloat:0.0f forKey:@"toolbarAlpha"];
+//    [defaults setFloat:0.5f forKey:@"userScrollSpeed"];
+//    [defaults setFloat:0.3f forKey:@"tbRed"];
+//    [defaults setFloat:0.3f forKey:@"tbGreen"];
+//    [defaults setFloat:0.3f forKey:@"tbBlue"];
+//    [defaults setFloat:1.0f forKey:@"textRed"];
+//    [defaults setFloat:1.0f forKey:@"textGreen"];
+//    [defaults setFloat:1.0f forKey:@"textBlue"];
+//    [defaults setFloat:0.0145f forKey:@"userZoomIncrement"];
+//    [defaults setFloat:.3 forKey:@"penSize"];
+//    [defaults setFloat:.2 forKey:@"eraserSize"];
+//    [defaults setValue:@"biology.png" forKey:@"testImage"];
+//    [defaults synchronize];
+//    
+//    // Set the starting view
+//    self.window.rootViewController = self.navigationController;
+//    [self.window makeKeyAndVisible];
+//    
+//    return YES;
+    
 }
-
-/**
- Release resources from memory
- */
 
 @end
--- AccessLecture/main.m
+++ AccessLecture/main.m
@@ -1,11 +1,15 @@
 // Copyright 2011 Access Lecture. All rights reserved.
 
 #import <UIKit/UIKit.h>
+#import "AccessLectureAppDelegate.h"
 
-int main(int argc, char *argv[]){
-    
+int main(int argc, char *argv[])
+{
     @autoreleasepool {
-        int retVal = UIApplicationMain(argc, argv, nil, nil);
-        return retVal;
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AccessLectureAppDelegate class]));
     }
+//    @autoreleasepool {
+//        int retVal = UIApplicationMain(argc, argv, nil, nil);
+//        return retVal;
+//    }
 }
