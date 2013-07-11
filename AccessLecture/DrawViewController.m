c0ee8f05a60404a66d31edc151e87c1b71e8bc8b
Piper Chester <piperchester@gmail.com>
2013-07-11 14:43:12 -0400

    Clamping DrawViewController's pan.

--- AccessLecture/DrawViewController.m
+++ AccessLecture/DrawViewController.m
@@ -75,48 +75,6 @@
     [_colorSegmentedControl setFrame:CGRectMake(0, 0, self.toolbarView.frame.size.width - 400, COLOR_HEIGHT)];
 }
 
-# pragma  mark - Gestures
-
-//- (void)panToMove:(UIPanGestureRecognizer *)gesture
-//{
-//    if ((gesture.state == UIGestureRecognizerStateChanged) ||
-//        (gesture.state == UIGestureRecognizerStateEnded)){
-//        
-//        CGPoint translation = [gesture translationInView:self.view];        
-//        _drawView.center = CGPointMake(_drawView.center.x + translation.x, _drawView.center.y + translation.y);
-//        
-//        // Clamp Left and Top Sides of View
-//        if (_drawView.frame.origin.x > 0){
-//            _drawView.frame = CGRectMake(0, _drawView.frame.origin.y, _drawView.frame.size.width, _drawView.frame.size.height);
-//        }
-//        
-//        if (_drawView.frame.origin.y > 0){
-//            _drawView.frame = CGRectMake(_drawView.frame.origin.x, 0, _drawView.frame.size.width, _drawView.frame.size.height);
-//        }
-//        
-//        // Clamp Right and Bottom Sides of View
-//        if (UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation)){
-//            if (_drawView.frame.origin.x < -500){
-//                _drawView.frame = CGRectMake(-500, _drawView.frame.origin.y, _drawView.frame.size.width, _drawView.frame.size.height);
-//            }
-//            
-//            if (_drawView.frame.origin.y < -1020){
-//                _drawView.frame = CGRectMake(_drawView.frame.origin.x, -1020, _drawView.frame.size.width, _drawView.frame.size.height);
-//            }
-//        } else if (UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].statusBarOrientation)){
-//            if (_drawView.frame.origin.x < -765){
-//                _drawView.frame = CGRectMake(-765, _drawView.frame.origin.y, _drawView.frame.size.width, _drawView.frame.size.height);
-//            } 
-//            
-//            if (_drawView.frame.origin.y < -1020){
-//                _drawView.frame = CGRectMake(_drawView.frame.origin.x, -1020, _drawView.frame.size.width, _drawView.frame.size.height);
-//            }
-//        }
-//        
-//        [gesture setTranslation:CGPointMake(0, 0) inView:self.view];
-//    }
-//}
-
 # pragma mark - Color Methods
 
 - (void)initColorSegmentedControl
