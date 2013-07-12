e54b573801dc04d0ad4f30dd17b5f671a4bd0364
7imbrook <timbrook480@gmail.com>
2013-07-12 16:16:43 -0400

    Travis CI scripts

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -e
 
-brew install xctool
+git clone https://github.com/facebook/xctool.git "xctool";
+cd xctool;
+git checkout 3677d7afad379366b571a3f634749c0fdad78368;
+cd ../;
+sh ./xctool/build.sh;
+export xctoolpath='./xctool/build/3677d7a/Products/Release/xctool';
