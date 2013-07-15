b41a048aa53f796a2d24e8abe3276cc97b080ab7
7imbrook <timbrook480@gmail.com>
2013-07-14 21:34:52 -0400

    Removed scripts

--- scripts/pre.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-git clone https://github.com/facebook/xctool.git "xctool";
-cd xctool;
-git checkout 3677d7afad379366b571a3f634749c0fdad78368;
-cd ../;
-sh ./xctool/build.sh;
-export xctoolpath='./xctool/build/3677d7a/Products/Release/xctool';
