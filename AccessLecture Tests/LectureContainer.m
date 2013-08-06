3f99c1bb25041cb52344cf9c69e7a219192fd6a7
7imbrook <timbrook480@gmail.com>
2013-08-06 13:46:53 -0400

    More test

--- AccessLecture Tests/LectureContainer.m
+++ AccessLecture Tests/LectureContainer.m
@@ -8,7 +8,20 @@
 
 #import <SenTestingKit/SenTestingKit.h>
 #import "LectureViewContainer.h"
+#import "DrawViewController.h"
 #import "ZoomBounds.h"
+
+@interface LectureViewContainer ()
+
+// Zoom pan test
+@property (nonatomic) CGPoint center;
+@property (nonatomic) CGSize space;
+@property (nonatomic) CGAffineTransform zoomLevel;
+@property (nonatomic) BOOL finish;
+
+- (void)applyTransforms;
+
+@end
 
 @interface LectureContainer : SenTestCase
 
@@ -36,13 +49,36 @@
     _testLVC = nil;
 }
 
-- (void)testChildren
+- (void)testChildrenCount
 {
-    
+    // Number of child view controllers should be 4
     STAssertTrue(_testLVC.childViewControllers.count == 4, @"Number of children is incorrect, %d returned", _testLVC.childViewControllers.count);
-    
-    
 }
 
+- (void)testVectors
+{
+    Vector vector = VectorMake(CGPointZero, CGPointMake(cos(1), sin(1)));
+    VectorApplyScale(2, &vector);
+    STAssertTrue(CGPointEqualToPoint(CGPointMake(2*cos(1), 2*sin(1)), vector.end), @"Points not equal");
+}
+
+- (void)testTransformsWithoutLimitBounce
+{
+    _testLVC.center = CGPointMake(500, 500);
+    _testLVC.finish = NO;
+    [_testLVC applyTransforms];
+    CGPoint testCenter = [(id<LectureViewChild>)[_testLVC.childViewControllers objectAtIndex:rand() % _testLVC.childViewControllers.count] contentView].center;
+    STAssertEquals(testCenter, CGPointMake(500, 500), @"View not transformed correctly");
+}
+
+- (void)testTransformsReturnToLimit
+{
+    _testLVC.center = CGPointMake(500, 0);
+    _testLVC.finish = YES;
+    [_testLVC applyTransforms];
+    UIView *cont = [(id<LectureViewChild>)[_testLVC.childViewControllers objectAtIndex:rand() % _testLVC.childViewControllers.count] contentView];
+    CGPoint testCenter = cont.center;
+    STAssertEquals(testCenter, CGPointMake(500, [UIScreen mainScreen].bounds.size.height - (cont.frame.size.height / 2)), @"View not transformed correctly");
+}
 
 @end
