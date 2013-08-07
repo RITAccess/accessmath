80b7304321a92b3fc99fd2c6314952fc4cc848e7
Piper Chester <piperchester@gmail.com>
2013-08-07 10:09:42 -0400

    Minor refactoring of AccessLectureRuntime.

--- AccessLecture/AccessLectureRuntime.m
+++ AccessLecture/AccessLectureRuntime.m
@@ -1,3 +1,4 @@
+
 //
 //  AccessLectureRuntime.m
 //  AccessLecture
@@ -11,6 +12,7 @@
 #import "AccessDocument.h"
 #import "FileManager.h"
 #import "Lecture.h"
+
 static NSString * DEFAULT_FILENAME = @"Lecture001"; 
 
 @interface AccessLectureRuntime ()
@@ -19,62 +21,60 @@ static NSString * DEFAULT_FILENAME = @"Lecture001";
 
 @implementation AccessLectureRuntime
 
-@synthesize currentDocument = _currentDocument;
-
-- (id)init {
+- (id)init
+{
     if (self = [super init]) {
-        // do initializing here
     }
     return self;
 }
+
 /**
- * Returns the global runtime object containing the current document
+ * Returns the global runtime object containing the current document.
  */
-+ (AccessLectureRuntime *)defaultRuntime {
-    static AccessLectureRuntime * defaults;
++ (AccessLectureRuntime *)defaultRuntime
+{
+    static AccessLectureRuntime *defaults;
     if (defaults) return defaults;
     defaults = [[AccessLectureRuntime alloc] init];
     return defaults;
 }
+
 /**
- * Opens the AccessDocument with the specified URL. The docuement is then attached to the default runtime
+ * Opens the AccessDocument with the specified URL. The docuement is then attached to the default runtime.
  * object.
  */
-- (void)openDocument:(NSURL *) withURL{
-    NSURL * currentDirectory = [FileManager iCloudDirectoryURL];
-    if (currentDirectory == nil) currentDirectory = [FileManager accessMathDirectoryURL];
-    NSArray * docs = [FileManager documentsIn:currentDirectory];
-       NSURL * document = [FileManager findFileIn:docs thatFits:^(NSURL* url){
-        if (url != nil) return YES;
+- (void)openDocument:(NSURL *) withURL
+{
+    NSURL *currentDirectory = [FileManager iCloudDirectoryURL];
+    if (currentDirectory == nil) {
+        currentDirectory = [FileManager accessMathDirectoryURL];
+    }
+    
+    NSArray *docs = [FileManager documentsIn:currentDirectory];
+    NSURL *document = [FileManager findFileIn:docs thatFits:^(NSURL* url){
+        if (url != nil) {
+            return YES;
+        }
         return NO;
     }];
+    
     if (document == nil) {
-        NSString * filename = [DEFAULT_FILENAME stringByAppendingPathExtension:[AccessDocument fileType]];
+        NSString *filename = [DEFAULT_FILENAME stringByAppendingPathExtension:[AccessDocument fileType]];
         document = [currentDirectory URLByAppendingPathComponent:filename];
-        }
+    }
+    
     _currentDocument = [[AccessDocument alloc] initWithFileURL:withURL];
     dispatch_barrier_async(dispatch_get_main_queue(), ^{
         [_currentDocument openWithCompletionHandler:^(BOOL success){
-            if(success)
-            {
-            [AccessLectureRuntime defaultRuntime].currentDocument = _currentDocument;
+            if(success) {
+                [AccessLectureRuntime defaultRuntime].currentDocument = _currentDocument;
+            } else {
+                [_currentDocument saveToURL:withURL forSaveOperation: UIDocumentSaveForCreating completionHandler:^(BOOL success) {
+                    success ? NSLog(@"Created!") : NSLog(@"Not created.");
+                }];
             }
-            else{
-                [_currentDocument saveToURL:withURL
-                           forSaveOperation: UIDocumentSaveForCreating
-                          completionHandler:^(BOOL success) {
-                              if (success){
-                                  NSLog(@"Created");
-                              } else {
-                                  NSLog(@"Not created");
-                              }
-                          }];
-            }
-            
         }];
-  
-  });
-    
-   }
+    });
+}
 
 @end
