bdd909d4f1940732661008bbb20e9554b0130563
Michael Timbrook <timbrook480@gmail.com>
2014-01-15 13:11:17 -0500

    reformated NTVC after rebase

--- AccessLecture/NoteTakingViewController.m
+++ AccessLecture/NoteTakingViewController.m
@@ -19,16 +19,13 @@
 @property UILabel *addNote;
 @property CGPoint menuPoint;
 @property (strong) MTFlowerMenu *menu;
-
-@end
-
-@interface NoteTakingViewController ()
-
 @property (strong) AMLecture *document;
 
 @end
 
 @implementation NoteTakingViewController
+
+#pragma mark Setup and Loading
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -70,6 +67,44 @@
 
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    // Get document
+    [[FileManager defaultManager] currentDocumentWithCompletion:^(AMLecture *lecture) {
+        _document = lecture;
+        for (Note *note in _document.lecture.notes) {
+            [self loadNoteAndPresent:note];
+        }
+    }];
+}
+
+
+#pragma mark - Note Creation and Viewing
+
+- (void)createTextNoteAndPresentAtPoint:(CGPoint)point
+{
+    TextNoteViewController *tnvc = [[TextNoteViewController alloc] initWithPoint:point];
+    [_document.lecture addNotes:[NSSet setWithObject:[(TextNoteView *)tnvc.view data]]];
+    [self addChildViewController:tnvc];
+    [self.view addSubview:tnvc.view];
+    tnvc.view.transform = CGAffineTransformMakeScale(0.0, 0.0);
+    [UIView animateWithDuration:0.4 delay:0.0 usingSpringWithDamping:0.6 initialSpringVelocity:0.2 options:UIViewAnimationCurveEaseInOut animations:^{
+        tnvc.view.transform = CGAffineTransformIdentity;
+    } completion:^(BOOL finished) {
+        [_document save];
+    }];
+}
+
+- (void)loadNoteAndPresent:(Note *)note
+{
+    TextNoteViewController *tnvc = [[TextNoteViewController alloc] initWithNote:note];
+    [self addChildViewController:tnvc];
+    [self.view addSubview:tnvc.view];
+    [_document save];
+}
+
+#pragma mark - Actions
+
 - (void)menuSelected:(MTFlowerMenu *)sender
 {
     if ([sender.selectedIdentifier isEqualToString:@"AddNote"]) {
@@ -84,91 +119,7 @@
     [self.view bringSubviewToFront:sender];
 }
 
-- (void)viewDidAppear:(BOOL)animated
-{
-    // Get document
-    [[FileManager defaultManager] currentDocumentWithCompletion:^(AMLecture *lecture) { 
-        _document = lecture;
-        for (Note *note in _document.lecture.notes) {
-            [self loadNoteAndPresent:note];
-        }
-    }];
-}
-
-- (void)recognizeWithTranslation:(CGPoint)translation
-{
-    // TODO add a modular system for adding menu items, look into UIDynamics for object colltion.
-    if (translation.y >= 45 && translation.y < 120 && translation.x < 75 && translation.x > -75) {
-        // Add note
-        [self createTextNoteAndPresentAtPoint:_menuPoint];
-    }
-}
-
-- (void)loadNoteAndPresent:(Note *)note
-{
-    TextNoteViewController *tnvc = [[TextNoteViewController alloc] initWithNote:note];
-    [self addChildViewController:tnvc];
-    [self.view addSubview:tnvc.view];
-    [_document save];
-}
-
-- (void)createTextNoteAndPresentAtPoint:(CGPoint)point
-{
-    TextNoteViewController *tnvc = [[TextNoteViewController alloc] initWithPoint:point];
-    [_document.lecture addNotes:[NSSet setWithObject:[(TextNoteView *)tnvc.view data]]];
-    [self addChildViewController:tnvc];
-    [self.view addSubview:tnvc.view];
-    tnvc.view.transform = CGAffineTransformMakeScale(0.0, 0.0);
-    [UIView animateWithDuration:0.4 delay:0.0 usingSpringWithDamping:0.6 initialSpringVelocity:0.2 options:UIViewAnimationCurveEaseInOut animations:^{
-        tnvc.view.transform = CGAffineTransformIdentity;
-    } completion:^(BOOL finished) {
-            [_document save];
-    }];
-}
-
-- (void)presentOptionsAtPoint:(CGPoint)point
-{
-    _addNote = ({
-        UILabel *label = [UILabel new];
-        CGRect frame = CGRectMake(point.x - 60, point.y - 10, 120, 20);
-        label.frame = frame;
-        label.alpha = 0.0;
-        label.text = @"Add Note";
-        label.textAlignment = NSTextAlignmentCenter;
-        [self.view addSubview:label];
-        label;
-    });
-    [UIView animateWithDuration:0.2 animations:^{
-        [_addNote setFrame:({
-            CGRect frame = _addNote.frame;
-            frame.origin.y = frame.origin.y - 50;
-            frame;
-        })];
-        [_addNote setAlpha:1.0];
-    }];
-}
-
-- (void)dismissOptions
-{
-    [UIView animateWithDuration:0.2 animations:^{
-        [_addNote setAlpha:0.0];
-    } completion:^(BOOL finished) {
-        [_addNote removeFromSuperview];
-    }];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (BOOL)shouldAutomaticallyForwardRotationMethods
-{
-    return YES;
-}
-
-#pragma mark Keyboard resonder
+#pragma mark Keyboard and Resonder
 
 - (void)keyboardDidShow:(NSNotification *)notification
 {
@@ -180,7 +131,12 @@
 
 }
 
-#pragma mark <LectureViewChild>
+- (BOOL)shouldAutomaticallyForwardRotationMethods
+{
+    return YES;
+}
+
+#pragma mark <LectureViewChild> - Deprecated?
 
 - (UIView *)contentView
 {
