374a2aa83c45f508f8f52b9c8b3e6c6383810ec2
piperchester <piperchester@gmail.com>
2014-05-29 14:40:57 -0400

    Add DrawViewTests file

---
+++ AccessLecture Tests/DrawViewTests.m
@@ -0,0 +1,46 @@
+//
+//  DrawViewTests.m
+//  AccessLecture
+//
+//  Created by Piper on 5/29/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+#import "DrawViewController.h"
+
+@interface DrawViewTests : XCTestCase
+
+@end
+
+@implementation DrawViewTests {
+    DrawViewController *drawViewController;
+}
+
+/**
+ *  Called before each test.
+ */
+- (void)setUp
+{
+    [super setUp];
+    drawViewController = [DrawViewController new];
+}
+
+/**
+ *  Put teardown code here. This method is called after the invocation of each test method in the class.
+ */
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+/**
+ *  Ensure that the DrawView toolbar is dismissed properly.
+ */
+- (void)testDrawViewToolbarHide
+{
+    [drawViewController dismissToolbarWithAnimation:NO];
+    XCTAssertTrue(drawViewController.toolbar.hidden = YES, @"Should be hidden.");
+}
+
+@end
