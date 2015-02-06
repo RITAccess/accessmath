9bed523de51c97d2c7be077abfb7f36117ae676c
Michael Timbrook <timbrook480@gmail.com>
2015-02-06 16:39:52 -0500

    Updated preview

---
+++ AccessLecture/PreviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  PreviewViewController.h
+//  AccessLecture
+//
+//  Created by Michael Timbrook on 2/6/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PreviewViewController : UIViewController
+
+@end
