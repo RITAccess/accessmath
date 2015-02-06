d3e3e437a6b16c994b483211678ba6b6205cf633
piperchester <piperchester@gmail.com>
2015-02-06 13:23:56 -0500

    Start rework

---
+++ AccessLecture/NoteListViewController.h
@@ -0,0 +1,13 @@
+//
+//  NoteListViewController.h
+//  AccessLecture
+//
+//  Created by Piper on 1/30/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NoteListViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
