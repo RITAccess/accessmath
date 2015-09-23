54e8bfc6ccdbd523197f6fc27f4bbe76b01bf78b
piperchester <piperchester@gmail.com>
2015-09-23 11:11:41 -0400

    Add NoteSearchResultsViewController

---
+++ AccessLecture/NoteSearchResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  NoteSearchResultsViewController.h
+//  AccessLecture
+//
+//  Created by Piper on 9/23/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NoteSearchResultsViewController : UIViewController
+
+@end
---
+++ AccessLecture/NoteSearchResultsViewController.m
@@ -0,0 +1,18 @@
+//
+//  NoteSearchResultsViewController.m
+//  AccessLecture
+//
+//  Created by Piper on 9/23/15.
+//
+//
+
+#import "NoteSearchResultsViewController.h"
+
+@implementation NoteSearchResultsViewController
+
+-(void)viewDidLoad
+{
+    NSLog(@"DEBUG: NoteSearchResultsViewController loaded.");
+}
+
+@end
