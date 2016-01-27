485b24cb8eb8b7987cdce087e6bc2dd390b5de4c
Michael Timbrook <timbrook480@gmail.com>
2016-01-27 12:42:31 -0500

    Started db setup and testing

--- AccessLecture/AMLecture.m
+++ AccessLecture/AMLecture.m
@@ -8,6 +8,16 @@
 
 #import "AMLecture.h"
 #import "AccessLectureKit.h"
+#import "FMDatabaseQueue.h"
+#import "FMDatabase.h"
+#import "Note.h"
+
+#define TEST 1
+
+#define DB_NAME @"notes.db"
+#define NOTE_TABLE @"notes"
+#define STATE_TABLE @"state"
+#define META_TABLE @"meta"
 
 static NSString *MetaKey = @"meta";
 static NSString *LectureKey = @"lecture";
@@ -15,10 +25,13 @@ static NSString *LectureKey = @"lecture";
 @interface AMLecture ()
 
 @property NSFileWrapper *fileWrapper;
+@property (readonly) NSString *databasePath;
 
 @end
 
-@implementation AMLecture
+@implementation AMLecture {
+     FMDatabaseQueue * _Nonnull _db;
+}
 
 - (id)initWithFileURL:(NSURL *)url
 {
@@ -31,6 +44,31 @@ static NSString *LectureKey = @"lecture";
         [_lecture setParent:self];
     }
     return self;
+}
+
+- (NSString *)databasePath {
+    static NSString *path;
+    if (!path) {
+        path = [self.fileURL.path stringByAppendingPathComponent:DB_NAME];
+    }
+    return path;
+}
+
+- (void)initDatabaseConnection {
+    if (![NSFileManager.defaultManager fileExistsAtPath:self.databasePath]) {
+        [NSFileManager.defaultManager createFileAtPath:self.databasePath contents:[NSData new] attributes:NULL];
+    }
+    _db = [FMDatabaseQueue databaseQueueWithPath:self.databasePath];
+    [_db inDatabase:^(FMDatabase *db) {
+        bool success = [db executeStatements:
+                        @"create table IF NOT EXISTS " NOTE_TABLE @" (title varchar(80), contents varchar(5000), modification REAL);"
+                        @"create table IF NOT EXISTS " STATE_TABLE @" (position varchar(80), modification REAL);"
+                        @"create table IF NOT EXISTS " META_TABLE @" (title varchar(80), date varchar(80) modification REAL);"
+
+                    ];
+        
+        NSLog(@"DEBUG: created %@", success ? @"YES" : @"NO");
+    }];
 }
 
 #pragma mark Encode and decode file wrappers
@@ -81,13 +119,11 @@ static NSString *LectureKey = @"lecture";
 
 - (id)contentsForType:(NSString *)typeName error:(NSError *__autoreleasing *)outError
 {
-
     NSMutableDictionary *wrappers = [NSMutableDictionary dictionary];
     [self encodeObject:self.metadata toWrappers:wrappers preferredFilename:@"lecture.meta"];
     [self encodeObject:self.lecture toWrappers:wrappers preferredFilename:@"lecture.data"];
     [self encodeImage:self.thumb toWrappers:wrappers preferredFilename:@"thumb.png"];
     NSFileWrapper *fileWrapper = [[NSFileWrapper alloc] initDirectoryWithFileWrappers:wrappers];
-    
     return fileWrapper;
 }
 
@@ -98,6 +134,10 @@ static NSString *LectureKey = @"lecture";
     self.metadata = nil;
     self.lecture = nil;
     self.thumb = nil;
+    [self initDatabaseConnection];
+#if TEST
+    NSLog(@"DEBUG: %@", [self testSomeCode]);;
+#endif
     return YES;
 }
 
@@ -154,7 +194,28 @@ static NSString *LectureKey = @"lecture";
     return [NSString stringWithFormat:@"AMLecture<%d> Title: '%@' number of notes %d", [super hash], _metadata.title, _lecture.notes.count];
 }
 
-- (NSArray *)getNotes
+#if TEST
+- (NSArray<Note *> *)testSomeCode {
+    __block FMResultSet *results;
+    [_db inDatabase:^(FMDatabase *db) {
+        [db executeUpdate:
+            @"INSERT into " NOTE_TABLE @" (title, contents) VALUES ('Test Note', 'Test not content that is resonably long but probably doesnt reach some edge case');"
+         ];
+        results = [db executeQuery:@"select * from " NOTE_TABLE];
+    }];
+    NSMutableArray<Note *> *resultsArray = [@[] mutableCopy];
+    while ([results next]) {
+        Note *n = [Note new];
+        [n setTitle:[results stringForColumn:@"title"]];
+        [n setContent:[results stringForColumn:@"contents"]];
+        [resultsArray addObject:n];
+    }
+    [results close];
+    return resultsArray;
+}
+#endif
+
+- (NSArray *)getNotes __deprecated_msg("Getting notes will require a state referance otherwise locations can't be populated. More info on how note listing will work. Will most likely be moved to Lecture.h and be a promise");
 {
     return _lecture.notes;
 }
