ac49ab87d620891749fa22488f8505fcc290442f
MukeytheMuse <kimsookoo121@gmail.com>
2016-03-03 14:41:25 -0500

    Added background files

---
+++ AccessLecture/BackgroundTile.h
@@ -0,0 +1,19 @@
+//
+//  BackgroundTile.h
+//  AccessLecture
+//
+//  Created by Kimberly Sookoo on 12/2/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface BackgroundTile : NSObject
+
+@property NSString *backgroundTileName;
+@property BOOL isColor;
+@property SKColor *colorName;
+
+
+@end
---
+++ AccessLecture/BackgroundTile.m
@@ -0,0 +1,14 @@
+//
+//  BackgroundTile.m
+//  AccessLecture
+//
+//  Created by Kimberly Sookoo on 12/2/15.
+//
+//
+
+#import "BackgroundTile.h"
+
+@implementation BackgroundTile
+
+
+@end
