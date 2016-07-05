d9cf70d657a2991cdcde58791cbf287fe6e069d4
MukeytheMuse <kimsookoo121@gmail.com>
2016-07-05 10:14:34 -0400

    Lectures are properly appearing on iPad and simulator

--- AccessLecture/AssignmentTableViewCell.m
+++ AccessLecture/AssignmentTableViewCell.m
@@ -19,6 +19,14 @@
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+    UIButton *infoButton = [UIButton buttonWithType:UIButtonTypeInfoLight];
+    infoButton.frame = CGRectMake(690,8,30, 30);
+    //[infoButton addTarget:self action:@selector(infoButtonClicked) forControlEvents:UIControlEventTouchUpInside];
+    [self.contentView addSubview:infoButton];
+}
+
+-(void)infoButtonSelected {
+    //Create a new view that contains the assignment's information in that view
 }
 
 @end
